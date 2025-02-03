70af644b822c1cc4d6c9a45c23d84166474356fd
Braxton Davis <braxton.davis@bsu.edu>
2025-02-02 23:41:54 -0500

    Add 15-puzzle to PuzzleConfig

--- src/search_problems/PuzzleConfig.java
+++ src/search_problems/PuzzleConfig.java
@@ -1,7 +1,7 @@
 package search_problems;
 
 public class PuzzleConfig {
-    public static final int[][] INITIAL_STATE = {
+ /*   public static final int[][] INITIAL_STATE = {
             {7, 2, 4},
             {5, 0, 6},
             {8, 3, 1}
@@ -13,4 +13,19 @@ public class PuzzleConfig {
             {7, 8, 0}
     };
 
-}
+  */
+        public static final int[][] INITIAL_STATE = {
+                {12, 1, 2, 15},
+                {11, 6, 5, 8},
+                {7, 10, 9, 4},
+                {0, 13, 14, 3}
+        };
+
+        public static final int[][] GOAL_STATE = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 0}
+        };
+    }
+
